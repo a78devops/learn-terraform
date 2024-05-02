9b9718ef4d9ddd4bf238fe9732dda737978d7af2
pajjuriabhishek <pajjuriabhishek55@gmail.com>
2024-05-02 12:26:14 +0530

    vars demo

--- 03-variables/var.tf
+++ 03-variables/var.tf
@@ -7,9 +7,28 @@ variable "x" {
 # declare variable without value
 #variable "y" {}
 
+variable "y_list" {
+  default = [1,2,3]
+}
+
+variable "z_map" {
+  default = {
+    x = 10
+    y = 20
+  }
+}
 
 # print variable
 output "x" {
   value = var.x
 }
+
+output "y" {
+  value = var.y_list
+}
+
+output "z" {
+  value = var.z_map
+}
+
 
