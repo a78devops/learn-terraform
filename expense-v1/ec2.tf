08fefd966ebfe38c5ec8f9a42af2588d7711a111
pajjuriabhishek <pajjuriabhishek55@gmail.com>
2024-05-04 18:13:17 +0530

    vars demo

--- expense-v1/ec2.tf
+++ expense-v1/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-090252cbe067a9e58"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0db858af714d96d69"]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_groups
   tags = {
     name = "frontend"
   }
@@ -10,9 +10,9 @@ resource "aws_instance" "frontend" {
 
 
 resource "aws_instance" "backend" {
-  ami           = "ami-090252cbe067a9e58"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0db858af714d96d69"]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_groups
   tags = {
     name = "backend"
   }
@@ -21,11 +21,22 @@ resource "aws_instance" "backend" {
 
 
 resource "aws_instance" "mysql" {
-  ami           = "ami-090252cbe067a9e58"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0db858af714d96d69"]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_groups
   tags = {
     name = "mysql"
   }
 }
 
+variable "ami" {
+  default = "ami-090252cbe067a9e58"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "security_groups" {
+  default = ["sg-0db858af714d96d69"]
+}
