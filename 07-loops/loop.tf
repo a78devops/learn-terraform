624a6b9c76dfd4609895d5231cc7d47113044298
pajjuriabhishek <pajjuriabhishek55@gmail.com>
2024-05-06 19:11:59 +0530

    loop demo

--- 07-loops/loop.tf
+++ 07-loops/loop.tf
@@ -4,3 +4,17 @@ resource "null_resource" "test" {
   count = 10
 }
 
+resource "null_resource" "test1" {
+  for_each = var.colours-codes
+}
+
+variable "colours-codes" {
+  default = {
+    red = 31
+    green = 32
+    yellow = 33
+    blue = 34
+    magenta = 35
+    cyan = 36
+  }
+}
